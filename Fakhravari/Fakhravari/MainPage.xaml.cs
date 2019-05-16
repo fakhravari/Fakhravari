d8ef847253ca0a2758a370362e22135b4536607b
mhf <fakhravary@gmail.com>
2019-05-17 00:50:18 +0430

    Add project files.

---
+++ Fakhravari/Fakhravari/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using Fakhravari.Class;
+using Fakhravari.Page;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace Fakhravari
+{
+    [DesignTimeVisible(true)]
+    public partial class MainPage : MasterDetailPage
+    {
+        public List<MasterPageItem> menuList { get; set; }
+        public MainPage()
+        {
+            InitializeComponent();
+
+            menuList = new List<MasterPageItem>
+            {
+                new MasterPageItem() { Title = "Home", Icon = "home.png", TargetType = typeof(HomePage) },
+                new MasterPageItem() { Title = "Setting", Icon = "setting.png", TargetType = typeof(SettingPage) },
+                new MasterPageItem() { Title = "Help", Icon = "help.png", TargetType = typeof(HelpPage) },
+                new MasterPageItem() { Title = "LogOut", Icon = "logout.png", TargetType = typeof(LogoutPage) }
+            };
+
+            navigationDrawerList.ItemsSource = menuList;
+
+            Detail = new NavigationPage((Xamarin.Forms.Page)Activator.CreateInstance(typeof(HomePage)));
+        }
+
+        private void OnMenuItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var item = (MasterPageItem)e.SelectedItem;
+            Type page = item.TargetType;
+
+            Detail = new NavigationPage((Xamarin.Forms.Page)Activator.CreateInstance(page));
+            IsPresented = false;
+        }
+    }
+}
